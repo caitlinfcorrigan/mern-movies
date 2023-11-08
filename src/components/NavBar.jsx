b831b2b66d7e8c807fdf02173859af4349b489a7
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-11-07 19:54:35 -0800

    adds navbar with links -- no username

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+    return (
+        <nav>
+            <a href='/'>Movies</a>
+            <a href='/actors'>Actors</a>
+            {/* Display the username here */}
+        </nav>
+    )
+}

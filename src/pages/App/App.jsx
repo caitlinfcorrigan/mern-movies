e6a2475b77796cc009c9e2e2452baf3d6be94b55
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-11-07 20:16:24 -0800

    passes addUser function to LoginPage, fixes routes in App, and adds BrowserRouter to index.js

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,6 +5,7 @@ import MovieDetailPage from '../MovieDetail/MovieDetailPage';
 import MoviesListPage from '../MoviesList/MoviesListPage';
 import ActorListPage from '../ActorList/ActorListPage';
 import LoginPage from '../Login/LoginPage';
+import NavBar from '../../components/NavBar';
 
 function App() {
   const [user, setUser] = useState();
@@ -16,16 +17,18 @@ function App() {
   return (
     <main className="App">
       { user ?
-        <Routes>
+        <>
           <NavBar/>
-          <Route path="/" element={<MoviesListPage />} />
-          <Route path="/movies/:movieName" element={<MovieDetailPage />} />
-          <Route path="/actors" element={<ActorListPage />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<MoviesListPage />} />
+            <Route path="/movies/:movieName" element={<MovieDetailPage />} />
+            <Route path="/actors" element={<ActorListPage />} />
+          </Routes>
+        </>
         :
         <Routes>
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
+          <Route path="/" element={<LoginPage addUser={ addUser } />} />
+        </Routes>
       }
     </main>
   );
--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-export default function LoginPage(){
-    const [username, setUsername] = useState("");
+export default function LoginPage({ addUser }){
+    const [username, setUsername] = useState({username:""});
 
     function handleUsername(e) {
-        const newUsername = { ...username, 'e.target.value'}
+        const newUsername = { ...username, [e.target.name]:e.target.value}
         setUsername(newUsername);
     }
 
@@ -14,12 +14,12 @@ export default function LoginPage(){
     }
 
     return(
-        <form>
+        <form onSumbit={handleLogin}>
             <label>Username</label>
             <input
                 type="text"
                 name="user"
-                value={username}
+                value={username.name}
                 onChange={handleUsername}
             />
             <button type="submit">Login</button>

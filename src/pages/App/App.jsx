75aefc17e5175637a8abd6109e6874280d3e2485
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-11-07 17:02:48 -0800

    implements ternairy expression for login and routes for pages

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MovieDetailPage from ('../MovieDetail/MovieDetailPage');
+import MoviesListPage from ('../MoviesList/MoviesListPage');
+import ActorListPage from ('../ActorList/ActorListPage');
+import LoginPage from ('../Login/LoginPage');
 
 function App() {
+  const [user, setUser] = useState();
   return (
     <main className="App">
-      Movie App
+      { user ?
+        <Routes>
+          <Route path="/movies" element={<MoviesListPage />} />
+          <Route path="/movies/detail" element={<MovieDetailPage />} />
+          <Route path="/actors" element={<ActorListPage />} />
+        </Routes>
+        :
+        <LoginPage/>
+      }
     </main>
   );
 }

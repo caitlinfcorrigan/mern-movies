779289b63715e65862d3ddf4146963dcce7f2f72
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-11-07 16:18:41 -0800

    creates basic, integrated MERN app

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+// Mount and configure serve-favicon & static MW
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+// Catch all route
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, function(){
+    console.log(`Express server running on ${port}`);
+});
